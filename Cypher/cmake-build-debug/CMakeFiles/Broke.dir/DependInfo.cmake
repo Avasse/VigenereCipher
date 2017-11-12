206953f45bb30b7ee900179a0f6c1567b9b73898
VASSE <alexandrevasse@laposte.net>
2017-11-12 18:42:50 +0100

    VIGENERE CYPHER : OK

--- Cypher/cmake-build-debug/CMakeFiles/Broke.dir/DependInfo.cmake
+++ Cypher/cmake-build-debug/CMakeFiles/Broke.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/XFR7254007/Desktop/work/dev/VigenereCypher/Broke/main.cpp" "C:/Users/XFR7254007/Desktop/work/dev/VigenereCypher/Broke/cmake-build-debug/CMakeFiles/Broke.dir/main.cpp.obj"
+  "C:/Users/utilisateur/Desktop/M1I2L/CRYPTO/VigenereCypher/Cypher/main.cpp" "C:/Users/utilisateur/Desktop/M1I2L/CRYPTO/VigenereCypher/Cypher/cmake-build-debug/CMakeFiles/Broke.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- Cypher/main.cpp
+++ Cypher/main.cpp
@@ -1,23 +1,26 @@
-//for (i = 1; i <= 26; i++){
-//for (j = 0; j <= tailleTexte; j+=k) {
-//int letterText = text[j] - 'A';
-//if (letterText == i) {
-//occured ++;
-//}
-//}
-//ind += (occured / tailleTexte) * ((occured - 1) / (tailleTexte - 1));
-//occured = 0;
-//}
-//if(indFinal < ind){
-//indFinal = ind;
-//kFinal = k;
-//}
-
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <iomanip>
+#include <algorithm>
+#include <fstream>
 using namespace std;
+
+//Took from EncryptDecrypt folder
+string decrypt(string text, string key){
+    string decryptedText;
+    int i = 0;
+    for(char c : text) {
+        if (isalpha(c)) {
+            c = toupper(c);
+            //decrypt the letter
+            decryptedText += ((c - 'A') - (key[i] - 'A') + 26) % 26 + 'A';
+            i++;
+            //Repeat the key
+            if (i == key.length()) i = 0;
+        } else decryptedText += c;
+    }
+    return decryptedText;
+}
 
 double calculInd (string text) {
     int i, j;
@@ -26,9 +29,6 @@ double calculInd (string text) {
         for (j = 0; j < tailleTexte; j++){
             if (text[j] == i) occured++;
         }
-        double test1 = occured / tailleTexte;
-        double test2 = (occured - 1) / (tailleTexte - 1);
-        double total = test1 * test2;
         ind += (occured / tailleTexte) * ((occured - 1) / (tailleTexte - 1));
         occured = 0;
     }
@@ -36,7 +36,7 @@ double calculInd (string text) {
 }
 
 int getKeyLenght(string text){
-    //On suppose que la taille de la clef est <=12;
+    //We suppose that the key length is <=12;
     int i, k;
     string text2, text3, text4, text5, text6, text7, text8, text9, text10, text11, text12;
     for(k = 2; k <= 12; k++) {
@@ -67,30 +67,86 @@ int getKeyLenght(string text){
         }
         keyLength++;
     }
+    return keyLengthFinal;
+}
 
-    cout << "Key length : " << keyLengthFinal << "ind : " << indFinal << endl;
+string getFinalKey (string text, int keyLength){
+    int i, j, k;
+    static int lettersArray[26];
+    string testText, key;
+    for(i = 0; i < keyLength; i++){
+        for(j = 0 + i; j < text.length(); j+= keyLength){
+            lettersArray[text[j] - 'A']++;
+        }
+        int maxOccuredLetter = distance(lettersArray, max_element(lettersArray, lettersArray+26)) + 65;
+        int shift = maxOccuredLetter - 'E';
+        // Workaround for  : maxOccurredLetter - 'E' mod26 with negativ values // See :
+        // https://stackoverflow.com/questions/12276675/modulus-with-negative-numbers-in-c
+        key += ((shift %26 + 26) %26) + 65;
+        //Assign 0 to each array element
+        fill(lettersArray, lettersArray + 26, 0);
+    }
+    return key;
 }
 
 int main() {
 
-    string text, line, cleanText; // Added this line
+    string firstText, secondText, line, firstCleanText, secondCleanText; // Added this line
     ifstream file;
-    file.open ("../first.txt");
+    file.open ("../texts/first.txt");
 
+    //Get first text
     while (!file.eof())
     {
         getline (file, line);
-        text+= line; // Added this line
+        firstText+= line; // Added this line
     }
     file.close();
-    for(char c : text) {
+    for(char c : firstText) {
         if (isalpha(c)) {
-            cleanText += toupper(c);
+            firstCleanText += toupper(c);
         }
     }
-    getKeyLenght(cleanText);
 
+    file.open ("../texts/second.txt");
 
+    //Get second text
+    while (!file.eof())
+    {
+        getline (file, line);
+        secondText+= line; // Added this line
+    }
+    file.close();
+    for(char c : secondText) {
+        if (isalpha(c)) {
+            secondCleanText += toupper(c);
+        }
+    }
+
+    int keyLenghtFirst = getKeyLenght(firstCleanText);
+    string keyFirst = getFinalKey(firstCleanText, keyLenghtFirst);
+
+    ofstream firstOutfile ("../premierTexteClair.txt");
+    firstOutfile << decrypt(firstText,keyFirst);
+    firstOutfile.close();
+
+    int keyLenghtSecond = getKeyLenght(secondCleanText);
+    string keySecond = getFinalKey(secondCleanText, keyLenghtSecond);
+
+    ofstream secondOutfile ("../deuxiemeTexteClair.txt");
+    secondOutfile << decrypt(secondText,keySecond);
+    secondOutfile.close();
+
+    cout << "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_HELLO THIS IS A VIGENERE CIPHER WITH 2 PREDEFINED TEXTS_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*" << endl << endl;
+    cout << "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_FIRST TEXT RESULTS_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*" << endl << endl;
+    cout << "KEY LENGTH : " << keyLenghtFirst << endl;
+    cout << "FINAL KEY : " << keyFirst << endl;
+    cout << "YOU CAN FIND THE DECRYPTED TEXT AT ./premierTexteClair.txt" << endl << endl;
+
+    cout << "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_SECOND TEXT RESULTS_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*" << endl << endl;
+    cout << "KEY LENGTH : " << keyLenghtSecond << endl;
+    cout << "FINAL KEY : " << keySecond << endl;
+    cout << "YOU CAN FIND THE DECRYPTED TEXT AT ./deuxiemeTexteClair.txt";
 
     return 0;
 }
